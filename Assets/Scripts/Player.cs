7a369f4bf7d1603cff5e4541f10461ddc88c07a6
菊池いつき <xxvraqwa@gmail.com>
2018-10-10 21:37:07 +0900

    タイルと敵追加

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class Player : MonoBehaviour
 {
-    [SerializeField] float walkSpeed = 50f;
-    [SerializeField] float runSpeed = 100f;
+    [SerializeField] float walkSpeed;
+    [SerializeField] float runSpeed;
     [SerializeField] float jump;
     Rigidbody2D rb = null;
     bool isGround = false;
@@ -27,17 +27,40 @@ public class Player : MonoBehaviour
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             rb.angularVelocity += speed;
+            if (!isGround && rb.velocity.x > -1f)
+            {
+                rb.velocity -= new Vector2(speed * 0.001f, 0f);
+            }
         }
 
         if (Input.GetKey(KeyCode.RightArrow))
         {
             rb.angularVelocity -= speed;
+            if (!isGround && rb.velocity.x < 1f)
+            {
+                rb.velocity += new Vector2(speed * 0.001f, 0f);
+            }
         }
 
         if (Input.GetKeyDown(KeyCode.Z) && isGround)
         {
-            rb.AddForce(new Vector2(0f, jump), ForceMode2D.Impulse);
+            rb.velocity = new Vector2(rb.velocity.x, jump);
         }
+
+        if (Input.GetKeyUp(KeyCode.Z))
+        {
+            rb.velocity -= new Vector2(0f, Mathf.Abs(rb.velocity.y / 2f));
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 size = new Vector2(Camera.main.orthographicSize * Camera.main.aspect, Camera.main.orthographicSize);
+
+        Vector3 cameraPosition = Vector3.Lerp(Camera.main.transform.position, this.transform.position, 0.1f);
+
+        cameraPosition = new Vector3(Mathf.Max(size.x, cameraPosition.x), Mathf.Max(size.y, cameraPosition.y), 0f);
+        Camera.main.transform.position = cameraPosition;
     }
 
     void OnCollisionEnter2D(Collision2D collision)
